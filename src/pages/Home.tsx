294d2a49e8f6eddedbf7a078a6c3c754f1cd16b0
Detriz-dev <kowalczyk.dawid@gmail.com>
2025-06-26 19:10:30 +0200

    Home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Home() {
+  return (
+    <div>
+      <h1>Hello React.</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
